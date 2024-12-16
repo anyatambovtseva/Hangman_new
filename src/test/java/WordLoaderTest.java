83c269c9d252ec04e7b8faa8aa2661acffcbed6b
Anya <anntambovtseva@mail.ru>
2024-12-17 01:00:43 +0500

    исправляю стилистические ошибки.

--- src/test/java/WordLoaderTest.java
+++ src/test/java/WordLoaderTest.java
@@ -1,27 +1,22 @@
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pavdvf.WordLoader;
-
 import java.io.IOException;
-
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class WordLoaderTest
-{
+public class WordLoaderTest {
     private WordLoader wordLoader;
     private final String testFilePath = "виселица.txt";
 
     @BeforeEach
-    public void setUp() throws IOException
-    {
+    public void setUp() throws IOException {
         wordLoader = new WordLoader();
     }
 
     @Test
-    public void testLoadWords() throws IOException
-    {
+    public void testLoadWords() throws IOException {
         wordLoader.loadWords(testFilePath);
 
         List<String> words = wordLoader.getWords();
