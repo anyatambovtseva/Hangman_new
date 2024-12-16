eb3917d6187106dffa4d2455f46ebccd676feadc
Anya <anntambovtseva@mail.ru>
2024-12-17 01:04:07 +0500

    исправляю стилистические ошибки.

--- src/test/java/GameLogicTest.java
+++ src/test/java/GameLogicTest.java
@@ -2,7 +2,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import pavdvf.GameLogic;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class GameLogicTest {
 
