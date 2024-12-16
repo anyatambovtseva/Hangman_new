e4d7fdebcd49825e7cc9b2957655940a31c3a21f
Anya <anntambovtseva@mail.ru>
2024-12-17 00:56:52 +0500

    исправляю стилистические ошибки.

--- src/main/java/pavdvf/Main.java
+++ src/main/java/pavdvf/Main.java
@@ -9,29 +9,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws TelegramApiException {
         Scanner scanner = new Scanner(System.in);
-        if(args[0].equals("telegram") && args.length != 0)
-        {
+        if (args[0].equals("telegram") && args.length != 0) {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             TGBot bot = new TGBot();
             botsApi.registerBot(bot);
             System.out.println("Телеграмм бот запущен!");
-        }
-        else if(args[0].equals("console") && args.length != 0)
-        {
+        } else if (args[0].equals("console") && args.length != 0) {
             new Thread(() -> new ConsoleApp(scanner).start()).start();
             System.out.println("Консольный бот запущен!");
-        }
-        else if(args[0].equals("console_AND_telegram") && args.length != 0)
-        {
+        } else if (args[0].equals("console_AND_telegram") && args.length != 0) {
             TelegramBotsApi botsApi = new TelegramBotsApi(DefaultBotSession.class);
             TGBot bot = new TGBot();
             botsApi.registerBot(bot);
             System.out.println("Телеграмм бот запущен!");
             new Thread(() -> new ConsoleApp(scanner).start()).start();
             System.out.println("Консольный бот запущен!");
-        }
-        else
-        {
+        } else {
             System.out.println("введите правильные аргументы!");
         }
     }
